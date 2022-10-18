cfa88212560034b036b90459b929d4e5d32613b9
Javarrisb <javarrisb@gmail.com>
2022-10-18 17:36:52 -0400

    gamecontroller

--- src/main/java/com/company/Summative1CristieJBNicholas/controller/GameController.java
+++ src/main/java/com/company/Summative1CristieJBNicholas/controller/GameController.java
@@ -5,10 +5,7 @@ import com.company.Summative1CristieJBNicholas.models.Games;
 import com.company.Summative1CristieJBNicholas.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,21 +15,44 @@ public class GameController {
     @Autowired
     GameRepository repo;
 
-    @GetMapping(value="/games/title")
+    @GetMapping(value="/games/game")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Games> getAllGames() {
+        return repo.findAll();
+    }
+
+    @GetMapping(value="/games/title/{title}")
     @ResponseStatus(HttpStatus.OK)
     public List<Games> getGamesbyTitle(@PathVariable String title)
     {return repo.findAllGamesByTitle(title);}
 
-    @GetMapping(value="/games/studio")
+    @GetMapping(value="/games/studio/{studio}")
     @ResponseStatus(HttpStatus.OK)
     public List<Games> getGamesByStudio(@PathVariable String studio){
         return repo.findAllGamesByStudio(studio);}
 
-    @GetMapping(value="/games/esrb_rating")
+    @GetMapping(value="/games/esrbRating/{esrbRating}")
     @ResponseStatus(HttpStatus.OK)
-    public List<Games> getGamesByEsrbRating(@PathVariable String esrb_rating){
-        return repo.findAllGamesByEsrbRating(esrb_rating);
+    public List<Games> getGamesByEsrbRating(@PathVariable String esrbRating){
+        return repo.findAllGamesByEsrbRating(esrbRating);
     }
 
+    @PostMapping(value="/games")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Games addGame(@RequestBody Games game) {
+        return repo.save(game);
+    }
+
+    @PutMapping(value="/games")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateGames(@RequestBody Games game) {
+        repo.save(game);
+    }
+
+    @DeleteMapping(value="/games/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteGame(@PathVariable Integer id){
+        repo.deleteById(id);
+    }
 
 }
