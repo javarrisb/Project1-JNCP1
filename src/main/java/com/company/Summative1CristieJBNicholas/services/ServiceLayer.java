49fc2d10eac52141e09b5647828e881191b59fc7
Criste Phillips <criste.phillips@yahoo.com>
2022-10-20 17:53:05 -0400

    restored testing functionality, games, tshirts

--- src/main/java/com/company/Summative1CristieJBNicholas/services/ServiceLayer.java
+++ src/main/java/com/company/Summative1CristieJBNicholas/services/ServiceLayer.java
@@ -1,43 +1,43 @@
-package com.company.Summative1CristieJBNicholas.services;
+//package com.company.Summative1CristieJBNicholas.services;
+//
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.stereotype.Service;
+//import com.company.Summative1CristieJBNicholas.models.Invoice;
+//import com.company.Summative1CristieJBNicholas.repository.InvoiceRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import javax.transaction.Transactional;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class ServiceLayer {
+//    private InvoiceRepository repo;
+//
+//    @Autowired
+//    public ServiceLayer(InvoiceRepository repo) {
+//        this.repo = repo;
+//    }
+//
+//    public Invoice createInvoice(Invoice invoice) {
+//        return repo.save(invoice);
+//    }
+//    public List<Invoice> findAll() {
+//        return repo.findAll();
+//    }
+//
+//    public void updateInvoice(Invoice invoice) {
+//        repo.save(invoice);
+//    }
+//
+//    public Optional<Invoice> findByID(int id) {
+//        Optional<Invoice> invoice = repo.findById(id);
+//        if (invoice.isPresent()) return invoice;
+//        return null;
+//    }
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Service;
-import com.company.Summative1CristieJBNicholas.models.Invoice;
-import com.company.Summative1CristieJBNicholas.repository.InvoiceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class ServiceLayer {
-    private InvoiceRepository repo;
-
-    @Autowired
-    public ServiceLayer(InvoiceRepository repo) {
-        this.repo = repo;
-    }
- 
-    public Invoice createInvoice(Invoice invoice) {
-        return repo.save(invoice);
-    }
-    public List<Invoice> findAll() {
-        return repo.findAll();
-    }
-
-    public void updateInvoice(Invoice invoice) {
-        repo.save(invoice);
-    }
-
-    public Optional<Invoice> findByID(int id) {
-        Optional<Invoice> invoice = repo.findById(id);
-        if (invoice.isPresent()) return invoice;
-        return null;
-    }
-
-     @Transactional
+//     @Transactional
 //    public AlbumViewModel saveAlbum(AlbumViewModel viewModel) {
 //        // write album information to the database
 //        Album a = new Album();
@@ -60,7 +60,7 @@ public class ServiceLayer {
 //            trackRepository.save(track);
 //        }
  
-}
+//}
 
 
 
